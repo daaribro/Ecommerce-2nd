f48976e038b94903456dc32dd4758b90c12ae920
daribro <npokharel409@gmail.com>
2020-06-01 23:42:21 +0545

    second try

--- core/models.py
+++ core/models.py
@@ -6,17 +6,12 @@ from django.shortcuts import reverse
 from django_countries.fields import CountryField
 
 
-CATEGORY_CHOICES = (
-    ('S', 'Shirt'),
-    ('SW', 'Sport wear'),
-    ('OW', 'Outwear')
+COMPANY_CHOICES = (
+    ('L', 'Lenovo'),
+    ('D', 'Dell'),
+    ('A', 'Acer')
 )
 
-LABEL_CHOICES = (
-    ('P', 'primary'),
-    ('S', 'secondary'),
-    ('D', 'danger')
-)
 
 ADDRESS_CHOICES = (
     ('B', 'Billing'),
@@ -38,8 +33,7 @@ class Item(models.Model):
     title = models.CharField(max_length=100)
     price = models.FloatField()
     discount_price = models.FloatField(blank=True, null=True)
-    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
-    label = models.CharField(choices=LABEL_CHOICES, max_length=1)
+    category = models.CharField(choices=COMPANY_CHOICES, max_length=2)
     slug = models.SlugField()
     description = models.TextField()
     image = models.ImageField()
